0eab6755b57fe287a75d21ab744c4316285c670d
Saulo Davi <saulodavi2001@gmail.com>
2022-03-21 00:32:49 -0300

    Aplicando React para atualizaçao da pagina

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import TelaInicial from "./TelaInicial"
 import TelaPerguntas from "./TelaPerguntas"
+import React from "react"
 
 export default function App(){
+    const [estadoPaginaUm, setEstadoPaginaUm] = React.useState(0)
 
+    if(estadoPaginaUm === 0){
+        return(
+            <>
+                <TelaInicial setEstadoPaginaUm = {setEstadoPaginaUm}/>
+            </>
+        )
+    }
+    if(estadoPaginaUm === 1 ){
     return(
         <>
             {/* <TelaInicial/> */}
             <TelaPerguntas/>
         </>
-    )
+    )}
 }
--- src/TelaPerguntas.js
+++ src/TelaPerguntas.js
@@ -1,15 +1,53 @@
-const perguntasINFO =    [{numPergunta: "1", texto: "PipipiPopopo"},
-                         {numPergunta: "2", texto: "PipiPopo"}]
+import React from "react"
+
+const perguntasINFO =    [{numPergunta: "1", pergunta: "Oque é JSX ?", resposta: "JSX é uma sintaxe para escrever HTML dentro do JS"},
+                          {numPergunta: "2", pergunta: "O React é ..."}]
 
 function Pergunta(props){
-  return(
-    <>
-      <button className="pergunta"> 
+
+  const [telaPergunta, setTelaPergunta] = React.useState(0);
+  const [perguntaRespondida, setPerguntaRespondida] = React.useState('')
+  const [nomeIcone, setNomeIcone] = React.useState("play-outline")
+
+  const classPrimeiraTelaCSS = `pergunta-primeira-tela ${perguntaRespondida}`
+  
+  if(telaPergunta === 0 ){
+    return(
+      <button className = {classPrimeiraTelaCSS} > 
         <p>Pergunta {props.numPergunta} </p>
-        <ion-icon className="icone-pergunta" name="play-outline"></ion-icon>
-      </button>
-    </>
-  )
+        <ion-icon onClick={ () => setTelaPergunta(1) } className="icone-pergunta" name={nomeIcone}></ion-icon>
+      </button>)}
+
+  if(telaPergunta === 1){
+    return(
+      <div className="pergunta-segunda-tela">
+        <p> {props.pergunta} </p>
+        <ion-icon onClick={() => setTelaPergunta(2)} className="icone-pergunta" name="repeat-outline"></ion-icon>
+      </div>)}
+
+  if(telaPergunta === 2){
+    return(
+      <div className="pergunta">
+      <div className="resposta-pergunta">
+        <p> {props.resposta} </p>
+        <div className="opcoes">
+          <div onClick = {() => {setTelaPergunta(0) 
+                                setPerguntaRespondida('resposta-nao')
+                                setNomeIcone('close-circle')}}
+                                            className="nao-lembrei"><p>Não lembrei!</p>
+          </div>
+          <div onClick = {() => {setTelaPergunta(0) 
+                                setPerguntaRespondida('resposta-quase')
+                                setNomeIcone('help-circle')}}
+                                            className="quase-nao-lembrei"><p>Quase não Lembrei</p>
+          </div>
+          <div onClick = {() => {setTelaPergunta(0) 
+                                setPerguntaRespondida('resposta-zap')
+                                setNomeIcone('checkmark-circle')}} className="zap"><p>Zap!</p>
+          </div>
+        </div>
+      </div>
+    </div>)}
 }
 
 export default function TelaPerguntas(){
@@ -19,7 +57,7 @@ export default function TelaPerguntas(){
                 <img src="ZapRecall-Recursos/logo.png" alt="logo"/>
                 <h1>ZapRecall</h1>
             </header>
-              {perguntasINFO.map(perguntaHTML => <Pergunta numPergunta={perguntaHTML.numPergunta}/>)}
+              {perguntasINFO.map(perguntaHTML => <Pergunta numPergunta={perguntaHTML.numPergunta} pergunta={perguntaHTML.pergunta}  resposta={perguntaHTML.resposta}/>)}
             <footer>
               <p>Y/X CONCLUÍDOS</p>
               </footer>
